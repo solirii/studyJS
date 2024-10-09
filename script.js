58345c53f52159be3ff892c3aed73fb51b30072c
solirii <solirii@bk.ru>
2024-10-10 01:11:47 +0300

    Исправлены ошибки в коде

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let title = prompt('Как называется ваш проект?');
 let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
-let screenPrice = prompt('Сколько будет стоить данная работа?', '12000');
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-let rollBack = fullPrice * (25 / 100);
+let screenPrice = +prompt('Сколько будет стоить данная работа?', '12000');
 let adaptive = confirm('Нужен ли адаптив на сайте?');
 let service1 = prompt('Какой дополнительный тип услуги нужен?')
-let servicePrice1 = prompt('Сколько это будет стоить?')
+let servicePrice1 = +prompt('Сколько это будет стоить?')
 let service2 = prompt('Какой дополнительный тип услуги нужен?')
-let servicePrice2 = prompt('Сколько это будет стоить?')
+let servicePrice2 = +prompt('Сколько это будет стоить?')
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let rollBack = fullPrice * (25 / 100);
 let servicePercentPrice = Math.ceil(fullPrice - rollBack)
 
 console.log(servicePercentPrice)
