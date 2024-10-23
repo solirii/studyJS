d47f9b2187ce701443a7316f37a1c13bc0389f27
solirii <solirii@bk.ru>
2024-10-23 21:56:16 +0300

    Добавлен пятый урок

--- script.js
+++ script.js
@@ -1,39 +1,72 @@
-let title = prompt('Как называется ваш проект?');
-let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
-let screenPrice = +prompt('Сколько будет стоить данная работа?', '12000');
-let adaptive = confirm('Нужен ли адаптив на сайте?');
-let service1 = prompt('Какой дополнительный тип услуги нужен?')
-let servicePrice1 = +prompt('Сколько это будет стоить?')
-let service2 = prompt('Какой дополнительный тип услуги нужен?')
-let servicePrice2 = +prompt('Сколько это будет стоить?')
+let title
+let screens
+let screenPrice
+let adaptive
 let fullPrice
 let rollBack = 10
 let servicePercentPrice
+let allServicePrices
+let service1
+let service2
 
+const isNumber = function (num) {
+    return !isNaN(parseFloat(num) && isFinite(num))
+}
 
-const allServicePrices = function getAllServicePrices() {
-    return servicePrice1 + servicePrice2
+const isString = function () {
+    return isNaN('')
+}
+
+const asking = function () {
+    title = prompt('Как называется ваш проект?')
+    screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные')
+
+    do {
+        screenPrice = prompt('Сколько будет стоить данная работа?')
+    }
+    while (!isNumber(screenPrice))
+
+    adaptive = confirm('Нужен ли адаптив на сайте?')
+}
+
+const getAllServicePrices = function () {
+    let sum = 0
+    for (let i = 0; i < 2; i++) {
+        let price = 0
+
+        if (i === 0) {
+            service1 = prompt('Какой дополнительный тип услуги нужен?')
+        } else if (i === 1) {
+            service2 = prompt('Какой дополнительный тип услуги нужен?')
+        }
+
+        do {
+            price = prompt('Сколько это будет стоить?')
+        } while (!isNumber(price))
+
+        sum += +price
+    }
+
+    return sum
+
+}
+
+const showTypeof = function (variable) {
+    console.log(variable, typeof variable)
+
 }
 
 function getFullPrice() {
-    return screenPrice + allServicePrices()
+    return +screenPrice + allServicePrices
 }
 
-fullPrice = getFullPrice()
-
-function getTitle() {
-    let firstChar = title.replace(/ /g, '').charAt(0)
-    let upperFirstChar = firstChar.toUpperCase()
-    let restOfTitle = title.slice(1).toLowerCase()
-    let newTitle = (upperFirstChar + restOfTitle)
-    return newTitle
+const getTitle = function () {
+    return title.trim()[0].toUpperCase() + title.trim().substr(1).toLowerCase()
 }
 
 function getServicePercentPrices() {
     return Math.ceil(fullPrice - (fullPrice * (rollBack / 100)))
 }
-
-servicePercentPrice = getServicePercentPrices()
 
 switch (true) {
     case fullPrice >= 30000:
@@ -51,17 +84,24 @@ switch (true) {
 
 }
 
+asking()
+allServicePrices = getAllServicePrices()
+fullPrice = getFullPrice()
+servicePercentPrice = getServicePercentPrices()
+title = getTitle()
+
+showTypeof(title)
+showTypeof(screenPrice)
+showTypeof(adaptive)
+
+console.log('allServicePrices', allServicePrices)
+
 console.log(typeof title)
 console.log(typeof screenPrice)
 console.log(typeof adaptive)
 
-
-console.log(fullPrice)
-
-console.log(getTitle())
-
+console.log(screens.length)
 console.log(servicePercentPrice)
 
 console.log('Стоимость верстки экранов' + " " + screenPrice + " " + 'рублей/ долларов/гривен/юани')
-
 console.log('Стоимость разработки сайта' + ' ' + fullPrice + ' ' + 'рублей/ долларов/гривен/юани')
